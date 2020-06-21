5c8cd88a5e18dc9364f29ecbc3384977ccad1909
Muhammad Sufyan <51535061+SamiSahai@users.noreply.github.com>
2020-06-21 07:45:35 -0700

    Update TransactionHistory.js
    included transaction maps

--- src/Components/TransactionHistory.js
+++ src/Components/TransactionHistory.js
@@ -1,25 +1,26 @@
-import React,{ useContext } from 'react';
+import React, { useContext } from 'react';
 
-// Import the Global Context
+// Import Transaction Component
+import { Transaction } from './Transaction';
+
+// Import the Global State
 import { GlobalContext } from '../context/GlobalState';
 
-//Import Transactions
-import { Transaction } from './Transaction'
 export const TransactionHistory = () => {
 
     const { transactions } = useContext(GlobalContext);
 
-
     return (
         <div>
-            <h3>Transaction History</h3>
+            <h3>
+                Transaction History
+            </h3>
             <ul className="list">
-                {transactions.map(transaction => (
+                {transactions.map(transaction => 
+                    (
                     <Transaction key={transaction.id} transaction={transaction} />
                     )
-                 )}
-                
-                
+                )}
             </ul>
         </div>
     )
