58ca0e13856b9f04f3899465755f996c1114be8d
Muhammad Sufyan <51535061+SamiSahai@users.noreply.github.com>
2020-06-21 07:40:40 -0700

    Update Transaction.js
    added data updating transactions

--- src/Components/Transaction.js
+++ src/Components/Transaction.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
+
+// Import the Global State
+import { GlobalContext } from '../context/GlobalState';
 
 export const Transaction = ({ transaction }) => {
-    return (
-        <li className="plus">
-            {Transaction.description}
-            <span>{Transaction.transactionAmount}</span>
-            <button className="delete-btn">x</button>
 
+    const { delTransaction } = useContext(GlobalContext);
+
+    const sign = transaction.transactionAmount > 0 ? '+' : '-';
+    const transactionType = transaction.transactionAmount > 0 ? 'plus' : 'minus';
+
+    return (
+        <li className={transactionType}>
+            {transaction.description}
+            <span>{sign}${Math.abs(transaction.transactionAmount)}</span>
+            <button className="delete-btn"
+                    onClick={() => delTransaction(transaction.id)}>
+                X
+            </button>
         </li>
     )
 }
