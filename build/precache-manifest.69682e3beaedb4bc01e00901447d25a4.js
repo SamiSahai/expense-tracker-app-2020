91099a32b65870c044aaca3906756ae8165d28f6
Muhammad Sufyan <51535061+SamiSahai@users.noreply.github.com>
2020-06-21 22:15:16 -0800

    updated app.js accountsummary and transactions
    updated essential global components

---
+++ build/precache-manifest.69682e3beaedb4bc01e00901447d25a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "787b3cfe6f9a543fc7a604b6dae64a15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "987088a98e9b9d278b98",
+    "url": "/static/css/main.dbb54315.chunk.css"
+  },
+  {
+    "revision": "eecac5fd83b7e26badf4",
+    "url": "/static/js/2.c007e4b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c007e4b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "987088a98e9b9d278b98",
+    "url": "/static/js/main.e4669b93.chunk.js"
+  },
+  {
+    "revision": "cb64f2e13d256d26a7ac",
+    "url": "/static/js/runtime-main.cdbb12e1.js"
+  }
+]);
